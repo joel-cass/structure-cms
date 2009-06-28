b8dc4492b49a96483da76f7a586f0c69b8d4901d
jozza <jozza@jozza.net>
2009-06-28 22:41:36 +0000

    Modified the installer to create an RSS tree

--- installer.php
+++ installer.php
@@ -4,6 +4,8 @@ require_once "classes/core/Page.php";
 if (!is_dir(getPath(""))) {
 	mkdir(getPath(""));
 }
+
+// Create website tree
 if (!Page::isPage("/home")) {
 	$home = Page::create("/home", "content");
 	$home->setField("Title", "Home");
@@ -17,6 +19,8 @@ if (!Page::isPage("/home/about")) {
 	$about->setField("Content", "<h2>Welcome to StructureCMS!</h2><p>Lorem ipsum dolor sit amet...</p>");
 	$about->save();
 }
+
+// Create footer tree
 if (!Page::isPage("/footer")) {
 	$footer = Page::create("/footer", "content");
 	$footer->setField("Title", "Footer");
@@ -26,6 +30,19 @@ if (!Page::isPage("/footer/contact")) {
 	$contact = Page::create("/footer/contact", "content");
 	$contact->setField("Title", "Contact");
 	$contact->setField("Content", "<h2>Welcome to StructureCMS!</h2><p>Lorem ipsum dolor sit amet...</p>");
+	$contact->save();
+}
+
+// Create RSS tree
+if (!Page::isPage("/rss")) {
+	$footer = Page::create("/rss", "content");
+	$footer->setField("Title", "RSS");
+	$footer->save();
+}
+if (!Page::isPage("/rss/latest")) {
+	$contact = Page::create("/rss/latest", "rss");
+	$contact->setField("Title", "Latest Items");
+	$contact->setField("Description", "Latest items created or modified on the site");
 	$contact->save();
 }
 
