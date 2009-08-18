854685f8e49c6a64a7c53e6f405b295fdf919c3e
jozza@jozza.net <jozza@jozza.net@9a87bef2-5ebb-11de-992f-fdd7e25b9163>
2009-08-18 14:11:00 +0000

    polish off installation

--- trunk/installer.php
+++ trunk/installer.php
@@ -11,7 +11,7 @@ if (!Page::isPage("/home")) {
 	$home->setField("Title", "Home");
 	$home->setField("Description", "Home page of a default installation of StructureCMS");
 	$home->setField("Keywords", "StructureCMS Content Management System");
-	$home->setField("Content", "<h2>Welcome to StructureCMS!</h2><p>Your site has been installed and is ready to use. The following links may be of assistance to you.</p><ul><li><a href=\"" . getRootURL() . "/admin\">Administration</a> (U:admin, p:yay)</li><li><a href=\"" . getRootURL() . "/content/rss/latest/\">RSS Feed</a></li></ul>");
+	$home->setField("Content", "<h2>Welcome to StructureCMS!</h2><p>Your site has been installed and is ready to use. The following links may be of assistance to you.</p><ul><li><a href=\"" . getRootURL() . "/admin\">Administration</a> (U:admin, p:yay)</li><li><a href=\"" . getRootURL() . "/content/footer/search/\">Search</a></li><li><a href=\"" . getRootURL() . "/content/footer/contact/\">Contact</a></li><li><a href=\"" . getRootURL() . "/content/rss/latest/\">RSS Feed</a></li></ul>");
 	$home->setLayout("home");
 	$home->save();
 }
@@ -20,7 +20,7 @@ if (!Page::isPage("/home/about")) {
 	$about->setField("Title", "About");
 	$about->setField("Description", "About StructureCMS");
 	$about->setField("Keywords", "StructureCMS Content Management System");
-	$about->setField("Content", "<h2>About StructureCMS</h2><p>For more information, please visit <a href=\"http://code.google.com/p/structure-cms/\">the project home page</a></p>");
+	$about->setField("Content", "<p>For more information, please visit <a href=\"http://code.google.com/p/structure-cms/\">the project home page</a></p>");
 	$about->save();
 }
 
@@ -35,7 +35,7 @@ if (!Page::isPage("/footer/contact")) {
 	$contact->setField("Title", "Contact");
 	$contact->setField("Description", "Contains a dummy contact form for testing purposes. Or you could actually use it.");
 	$contact->setField("Keywords", "StructureCMS Content Management System");
-	$contact->setField("Content", "<h2>Contact Form</h2><p>No email address has been entered. You will need to enter one.</p>");
+	$contact->setField("Content", "<p>No email address has been entered. You will need to enter one.</p>");
 	$contact->save();
 }
 if (!Page::isPage("/footer/search")) {
@@ -43,7 +43,7 @@ if (!Page::isPage("/footer/search")) {
 	$search->setField("Title", "Search");
 	$search->setField("Description", "A simple text-based file crawling search engine.");
 	$search->setField("Keywords", "StructureCMS Content Management System");
-	$search->setField("Content", "<h2>Search</h2><p>Enter a keyword, see if it turns up.</p>");
+	$search->setField("Content", "<p>Enter a keyword, see if it turns up.</p>");
 	$search->setPlaceHolderViews('main',array('main/title_and_text.php','main/search.php'));
 	$search->save();
 }
