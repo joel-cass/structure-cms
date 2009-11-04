1b2edd913d1bea56f6d314f920abfae4c5bddd5a
joel.a.cass <joel.a.cass@9a87bef2-5ebb-11de-992f-fdd7e25b9163>
2009-11-04 05:15:39 +0000

    Added styles - work in progress

--- cms/layouts/default.php
+++ cms/layouts/default.php
@@ -6,33 +6,36 @@
 	<meta name="Keywords" content="<?php LayoutHelper::renderField($PAGE, "Keywords"); ?>">
 	<meta name="Description" content="<?php LayoutHelper::renderField($PAGE, "Description"); ?>">
 	<title><?php LayoutHelper::renderField($PAGE, "Title"); ?></title>
-	<link href="<?php echo getRootURL(); ?>/jnet/lib/style.css" rel="stylesheet" type="text/css">
-	<script src="<?php echo getRootURL(); ?>/jnet/lib/suckerfish.js" type="text/javascript"></script>
+	<link href="<?php echo getRootURL(); ?>/styles/classic/lib/style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 	<div id="page-wrapper">
-		<div id="site-name">
-			<span>StructureCMS</span>
-		</div>
-		<div id="navigation">
-			<?php include "includes/nav.php" ?>
-		</div>
-		<div id="content-wrapper">
-			<div id="side-nav">
-				<?php include "includes/side-nav.php" ?>
+		<div id="page">
+			<div id="site-name">
+				<span>StructureCMS</span>
 			</div>
-			<div id="content">
+			<div id="navigation">
+				<?php include "includes/nav.php" ?>
+			</div>
+			<div id="navigation-footer"></div>
+			<div id="content-wrapper">
+				<div id="side-nav">
+					<?php include "includes/side-nav.php" ?>
+				</div>
 				<div id="breadcrumb">
 					<?php include "includes/bread-crumb.php" ?>
 				</div>
-				<?php LayoutHelper::renderPlaceHolder($PAGE, "Main"); ?>
+				<div id="content">
+					<?php LayoutHelper::renderPlaceHolder($PAGE, "Main"); ?>
+				</div>
+				<div class="clear-hidden"></div>
 			</div>
-			<div class="clear-hidden"></div>
-		</div>
-		<div id="footer">
-			<?php include "includes/footer.php" ?>
-			<div class="clear-hidden"></div>
+			<div id="footer">
+				<?php include "includes/footer.php" ?>
+				&copy; 2009 Joel Cass
+				<div class="clear-hidden"></div>
+			</div>
 		</div>
 	</div>
 </body>
